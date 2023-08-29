804d32193257fbf4f743a0094e70b0bfce1fb998
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2023-08-29 20:26:08 +0900

    TimeLineをTimelineに統一

--- lib/providers.dart
+++ lib/providers.dart
@@ -21,7 +21,7 @@ import 'package:miria/repository/local_time_line_repository.dart';
 import 'package:miria/repository/role_timeline_repository.dart';
 import 'package:miria/repository/note_repository.dart';
 import 'package:miria/repository/tab_settings_repository.dart';
-import 'package:miria/repository/time_line_repository.dart';
+import 'package:miria/repository/timeline_repository.dart';
 import 'package:miria/repository/user_list_time_line_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:miria/state_notifier/common/misskey_server_list_notifier.dart';
@@ -45,7 +45,7 @@ final localTimeLineProvider =
     ChangeNotifierProvider.family<TimelineRepository, TabSetting>(
         (ref, tabSetting) {
   final account = ref.watch(accountProvider(tabSetting.acct));
-  return LocalTimeLineRepository(
+  return LocalTimelineRepository(
     ref.read(misskeyProvider(account)),
     account,
     ref.read(notesProvider(account)),
@@ -62,7 +62,7 @@ final homeTimeLineProvider =
     ChangeNotifierProvider.family<TimelineRepository, TabSetting>(
         (ref, tabSetting) {
   final account = ref.watch(accountProvider(tabSetting.acct));
-  return HomeTimeLineRepository(
+  return HomeTimelineRepository(
     ref.read(misskeyProvider(account)),
     account,
     ref.read(notesProvider(account)),
@@ -79,7 +79,7 @@ final globalTimeLineProvider =
     ChangeNotifierProvider.family<TimelineRepository, TabSetting>(
         (ref, tabSetting) {
   final account = ref.watch(accountProvider(tabSetting.acct));
-  return GlobalTimeLineRepository(
+  return GlobalTimelineRepository(
     ref.read(misskeyProvider(account)),
     ref.read(notesProvider(account)),
     ref.read(mainStreamRepositoryProvider(account)),
