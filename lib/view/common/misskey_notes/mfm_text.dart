4d785b7546a41c43ec3aa51a0dfab3a9c1052dee
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2023-09-04 18:57:50 +0900

    省略されたノートを一定の行まで表示する

--- lib/view/common/misskey_notes/mfm_text.dart
+++ lib/view/common/misskey_notes/mfm_text.dart
@@ -33,6 +33,8 @@ class MfmText extends ConsumerStatefulWidget {
   final List<InlineSpan> prefixSpan;
   final Function(MisskeyEmojiData)? onEmojiTap;
   final bool isEnableAnimatedMFM;
+  final TextOverflow? overflow;
+  final int? maxLines;
 
   const MfmText({
     super.key,
@@ -46,6 +48,8 @@ class MfmText extends ConsumerStatefulWidget {
     this.prefixSpan = const [],
     this.onEmojiTap,
     this.isEnableAnimatedMFM = true,
+    this.overflow,
+    this.maxLines,
   }) : assert(mfmText != null || mfmNode != null);
 
   @override
@@ -142,6 +146,8 @@ class MfmTextState extends ConsumerState<MfmText> {
       suffixSpan: widget.suffixSpan,
       prefixSpan: widget.prefixSpan,
       isUseAnimation: widget.isEnableAnimatedMFM,
+      overflow: widget.overflow,
+      maxLines: widget.maxLines,
     );
   }
 }
