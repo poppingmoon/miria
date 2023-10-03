3f735c99f0aeae8135c1a467079559216cbeda82
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2023-10-03 19:29:41 +0900

    ファイル名変更

--- lib/view/note_create_page/create_file_view.dart
+++ lib/view/note_create_page/create_file_view.dart
@@ -3,7 +3,7 @@ import 'dart:typed_data';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:miria/model/image_file.dart';
+import 'package:miria/model/misskey_post_file.dart';
 import 'package:miria/providers.dart';
 import 'package:miria/router/app_router.dart';
 import 'package:miria/view/common/account_scope.dart';
