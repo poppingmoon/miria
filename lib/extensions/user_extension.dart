2b2977ef34965a5f485924a8a4b97dec8c53924f
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2023-10-15 17:51:23 +0900

    非公開のときフォロワー数を表示しない

--- lib/extensions/user_extension.dart
+++ lib/extensions/user_extension.dart
@@ -5,3 +5,20 @@ extension UserExtension on User {
     return "@$username${host != null ? "@$host" : ""}";
   }
 }
+
+extension UserDetailedExtension on UserDetailed {
+  bool get isFfVisibleForMe {
+    if (this is MeDetailed) {
+      return true;
+    }
+
+    final user = this;
+
+    return switch (ffVisibility) {
+      FFVisibility.public => true,
+      FFVisibility.followers =>
+        user is UserDetailedNotMeWithRelations && user.isFollowing,
+      FFVisibility.private => false,
+    };
+  }
+}
