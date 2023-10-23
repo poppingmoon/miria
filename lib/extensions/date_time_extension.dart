3e3d2a9e1b24bc020057735cf8a9eb28aeff81ce
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2023-10-24 05:03:24 +0900

    DriveFolderModalSheetを追加

--- lib/extensions/date_time_extension.dart
+++ lib/extensions/date_time_extension.dart
@@ -12,6 +12,9 @@ extension DateTimeExtension on DateTime {
   String get formatUntilMinutes =>
       DateFormat("yyyy 年 M 月 d 日 HH:mm").format(toUtc().toLocal());
 
+  String get formatUntilSeconds =>
+      DateFormat("yyyy/MM/dd HH:mm:ss").format(toUtc().toLocal());
+
   String get formatUntilMilliSeconds =>
       "${DateFormat("yyyy/MM/dd HH:mm:ss", "ja_jp").format(toUtc().toLocal())}.${millisecond.toString().padLeft(3, '0')}";
 
