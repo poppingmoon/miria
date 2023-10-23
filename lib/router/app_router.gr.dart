7aad8de091e5bea8b345c5c451475e6537de01e6
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2023-10-24 06:08:14 +0900

    フォルダとファイルを移動できるように

--- lib/router/app_router.gr.dart
+++ lib/router/app_router.gr.dart
@@ -428,6 +428,8 @@ abstract class _$AppRouter extends RootStackRouter {
         child: DrivePage(
           key: args.key,
           account: args.account,
+          title: args.title,
+          floatingActionButtonBuilder: args.floatingActionButtonBuilder,
         ),
       );
     },
@@ -1948,12 +1950,16 @@ class DriveRoute extends PageRouteInfo<DriveRouteArgs> {
   DriveRoute({
     Key? key,
     required Account account,
+    Widget? title,
+    Widget Function(BuildContext)? floatingActionButtonBuilder,
     List<PageRouteInfo>? children,
   }) : super(
           DriveRoute.name,
           args: DriveRouteArgs(
             key: key,
             account: account,
+            title: title,
+            floatingActionButtonBuilder: floatingActionButtonBuilder,
           ),
           initialChildren: children,
         );
@@ -1967,15 +1973,21 @@ class DriveRouteArgs {
   const DriveRouteArgs({
     this.key,
     required this.account,
+    this.title,
+    this.floatingActionButtonBuilder,
   });
 
   final Key? key;
 
   final Account account;
 
+  final Widget? title;
+
+  final Widget Function(BuildContext)? floatingActionButtonBuilder;
+
   @override
   String toString() {
-    return 'DriveRouteArgs{key: $key, account: $account}';
+    return 'DriveRouteArgs{key: $key, account: $account, title: $title, floatingActionButtonBuilder: $floatingActionButtonBuilder}';
   }
 }
 
