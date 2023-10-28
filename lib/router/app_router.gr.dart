6d157be02663ea21e19200757432e24db55c0e71
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2023-10-28 17:56:25 +0900

    ファイルの複数選択を追加

--- lib/router/app_router.gr.dart
+++ lib/router/app_router.gr.dart
@@ -430,6 +430,7 @@ abstract class _$AppRouter extends RootStackRouter {
           account: args.account,
           title: args.title,
           floatingActionButtonBuilder: args.floatingActionButtonBuilder,
+          tapToSelect: args.tapToSelect,
         ),
       );
     },
@@ -1952,6 +1953,7 @@ class DriveRoute extends PageRouteInfo<DriveRouteArgs> {
     required Account account,
     Widget? title,
     Widget Function(BuildContext)? floatingActionButtonBuilder,
+    bool tapToSelect = false,
     List<PageRouteInfo>? children,
   }) : super(
           DriveRoute.name,
@@ -1960,6 +1962,7 @@ class DriveRoute extends PageRouteInfo<DriveRouteArgs> {
             account: account,
             title: title,
             floatingActionButtonBuilder: floatingActionButtonBuilder,
+            tapToSelect: tapToSelect,
           ),
           initialChildren: children,
         );
@@ -1975,6 +1978,7 @@ class DriveRouteArgs {
     required this.account,
     this.title,
     this.floatingActionButtonBuilder,
+    this.tapToSelect = false,
   });
 
   final Key? key;
@@ -1985,9 +1989,11 @@ class DriveRouteArgs {
 
   final Widget Function(BuildContext)? floatingActionButtonBuilder;
 
+  final bool tapToSelect;
+
   @override
   String toString() {
-    return 'DriveRouteArgs{key: $key, account: $account, title: $title, floatingActionButtonBuilder: $floatingActionButtonBuilder}';
+    return 'DriveRouteArgs{key: $key, account: $account, title: $title, floatingActionButtonBuilder: $floatingActionButtonBuilder, tapToSelect: $tapToSelect}';
   }
 }
 
