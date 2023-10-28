61bec64f1e29d51baadd06b95124a63486e27429
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2023-10-28 18:14:25 +0900

    formatUntilSecondsをformatUntilMinutesに変更

--- lib/view/note_create_page/vote_area.dart
+++ lib/view/note_create_page/vote_area.dart
@@ -236,7 +236,7 @@ class VoteUntilDateState extends ConsumerState<VoteUntilDate> {
                     const Padding(padding: EdgeInsets.only(left: 10)),
                     Expanded(
                       child: Text(
-                        date?.formatUntilSeconds ?? "",
+                        date?.formatUntilMinutes ?? "",
                       ),
                     ),
                   ]),
