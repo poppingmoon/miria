5b5fa0e0dd38d6e7103aeff2451cd5a459cd5b98
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2023-10-28 17:56:25 +0900

    ファイルの複数選択を追加

--- lib/view/drive_page/drive_selected_files_modal_sheet.dart
+++ lib/view/drive_page/drive_selected_files_modal_sheet.dart
@@ -1,44 +1,37 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:miria/extensions/date_time_extension.dart';
 import 'package:miria/model/account.dart';
 import 'package:miria/providers.dart';
 import 'package:miria/view/common/error_dialog_handler.dart';
-import 'package:miria/view/common/text_form_field_dialog.dart';
 import 'package:miria/view/dialogs/simple_confirm_dialog.dart';
 import 'package:miria/view/drive_page/drive_folder_select_dialog.dart';
 import 'package:misskey_dart/misskey_dart.dart';
 
-class DriveFolderModalSheet extends ConsumerWidget {
-  const DriveFolderModalSheet({
+class DriveSelectedFilesModalSheet extends ConsumerWidget {
+  const DriveSelectedFilesModalSheet({
     super.key,
     required this.account,
-    required this.folder,
+    required this.files,
   });
 
   final Account account;
-  final DriveFolder folder;
+  final List<DriveFile> files;
 
-  Future<void> changeName(WidgetRef ref) async {
+  Future<void> download(WidgetRef ref) async {
     final context = ref.context;
-    final misskey = ref.read(misskeyProvider(account));
-    final name = await showDialog<String>(
-      context: context,
-      builder: (context) => TextFormFieldDialog(
-        title: const Text("名前を変更"),
-        labelText: "フォルダ名",
-        initialValue: folder.name,
+    await Future.wait(
+      files.map(
+        (file) =>
+            ref.read(downloadFileNotifierProvider.notifier).downloadFile(file),
       ),
     );
-    if (name != null && name != folder.name) {
-      await ref
-          .read(
-            driveFoldersNotifierProvider((misskey, folder.parentId)).notifier,
-          )
-          .updateName(folder.id, name);
-      if (!context.mounted) return;
-      Navigator.of(context).pop();
-    }
+    if (!context.mounted) return;
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text("ファイルを保存しました")),
+    );
+    Navigator.of(context).pop();
   }
 
   Future<void> move(WidgetRef ref) async {
@@ -49,12 +42,16 @@ class DriveFolderModalSheet extends ConsumerWidget {
       builder: (context) => DriveFolderSelectDialog(account: account),
     );
     if (result == null) return;
-    await ref
-        .read(driveFoldersNotifierProvider((misskey, folder.parentId)).notifier)
-        .move(
-          folderId: folder.id,
-          parentId: result.$1?.id,
-        );
+    await Future.wait(
+      files.map(
+        (file) => ref
+            .read(driveFilesNotifierProvider((misskey, file.folderId)).notifier)
+            .move(
+              fileId: file.id,
+              folderId: result.$1?.id,
+            ),
+      ),
+    );
     if (!context.mounted) return;
     ScaffoldMessenger.of(context).showSnackBar(
       const SnackBar(content: Text("移動しました")),
@@ -67,17 +64,20 @@ class DriveFolderModalSheet extends ConsumerWidget {
     final misskey = ref.read(misskeyProvider(account));
     final result = await SimpleConfirmDialog.show(
       context: context,
-      message: "このフォルダを削除しますか？",
+      message: "${files.length}個のファイルを削除しますか？",
       primary: "削除する",
       secondary: "やめる",
     );
     if (result ?? false) {
-      await ref
-          .read(
-            driveFoldersNotifierProvider((misskey, folder.parentId)).notifier,
-          )
-          .delete(folder.id);
-      ref.read(drivePageNotifierProvider.notifier).deselectAll();
+      await Future.wait(
+        files.map(
+          (file) => ref
+              .read(
+                driveFilesNotifierProvider((misskey, file.folderId)).notifier,
+              )
+              .delete(file.id),
+        ),
+      );
       if (!context.mounted) return;
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text("削除しました")),
@@ -91,19 +91,13 @@ class DriveFolderModalSheet extends ConsumerWidget {
     return ListView(
       shrinkWrap: true,
       children: [
-        ListTile(
-          leading: const Icon(
-            Icons.folder,
-            size: 50,
+        ListTile(title: Text("${files.length}個のファイル")),
+        if (Platform.isAndroid || Platform.isIOS)
+          ListTile(
+            leading: const Icon(Icons.download),
+            title: const Text("ダウンロード"),
+            onTap: () => download(ref).expectFailure(context),
           ),
-          title: Text(folder.name),
-          subtitle: Text(folder.createdAt.formatUntilSeconds),
-        ),
-        ListTile(
-          leading: const Icon(Icons.settings),
-          title: const Text("名前を変更"),
-          onTap: () => changeName(ref).expectFailure(context),
-        ),
         ListTile(
           leading: const Icon(Icons.drive_file_move),
           title: const Text("移動"),
