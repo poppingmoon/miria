1a428743252761f50bf143e41c88476493594b39
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2023-11-06 07:30:26 +0900

    remove empty line

--- lib/view/note_create_page/vote_area.dart
+++ lib/view/note_create_page/vote_area.dart
@@ -4,7 +4,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:miria/extensions/date_time_extension.dart';
 import 'package:miria/providers.dart';
 import 'package:miria/state_notifier/note_create_page/note_create_state_notifier.dart';
-
 import 'package:miria/view/common/account_scope.dart';
 
 class VoteArea extends ConsumerStatefulWidget {
